feaa50d612f1c10949afd3430c2a31bb7863b12e
Aleksander Chromik <achromik@gmail.com>
2018-12-29 19:45:58 +0100

    Add: operators pads

--- App.js
+++ App.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
+const DELETE_OPERATOR = '⌫';
+const ADDITION_OPERATOR = '+';
+const SUBTRACTION_OPERATOR = '-';
+const MULTIPLICATION_OPERATOR = '*';
+const DIVISION_OPERATOR = '/';
+const CALCULATE_OPERATOR = '=';
+const DOT = '.';
+const ZERO = '0';
+
 export default class App extends Component {
     state = {
         calculationText: '',
         resultText: '',
     };
 
+    numbersPads = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9'],
+        [DOT, ZERO, CALCULATE_OPERATOR],
+    ];
+
+    operatorsPads = [
+        DELETE_OPERATOR,
+        ADDITION_OPERATOR,
+        SUBTRACTION_OPERATOR,
+        MULTIPLICATION_OPERATOR,
+        DIVISION_OPERATOR,
+    ];
+    handlePadPressed = text => {
+    };
     renderNumbersPad = () => {
-        const numbers = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['.', '0', '=']];
-        return numbers.map((row, index) => (
+        const numbersPads = this.numbersPads;
+        return numbersPads.map((row, index) => (
             <View key={index} style={styles.row}>
                 {row.map(key => (
-                    <TouchableOpacity style={styles.pad} key={key}>
+                    <TouchableOpacity
+                        style={styles.pad}
+                        key={key}
+                        onPress={() => this.handlePadPressed(key)}
+                    >
                         <Text style={styles.padText}>{key}</Text>
                     </TouchableOpacity>
                 ))}
             </View>
+        ));
+    };
+
+    renderOperatorsPad = () => {
+        const operatorsPads = this.operatorsPads;
+        return operatorsPads.map((pad, index) => (
+            <TouchableOpacity
+                style={styles.pad}
+                key={index}
+                onPress={() => this.handlePadPressed(pad)}
+            >
+                <Text style={styles.padText}>{pad}</Text>
+            </TouchableOpacity>
         ));
     };
 
@@ -33,7 +75,7 @@ export default class App extends Component {
                 </View>
                 <View style={styles.keyboard}>
                     <View style={styles.numbers}>{this.renderNumbersPad()}</View>
-                    <View style={styles.operators} />
+                    <View style={styles.operators}>{this.renderOperatorsPad()}</View>
                 </View>
             </View>
         );
